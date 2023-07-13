57450de309a524f2f2931019f4c6ad9141407062
MsSelly <syskhah@gmail.com>
2023-07-13 03:19:22 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,58 @@
 #include "main.h"
+#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
- * read_textfile - This Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - this function reads and prints a file
+ * @filename: storage of file
+ * @letters: holds the number of letters
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: actual number of letters read
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fdes;
 	char *buffer;
+	ssize_t reads, text;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	fdes = open(filename, O_RDONLY);
+	if (fdes == -1)
 	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(fdes);
+		return (0);
+	}
+
+	reads = read(fdes, buffer, letters);
+	if (reads == -1)
+	{
+		close(fdes);
 		free(buffer);
 		return (0);
 	}
 
-	free(buffer);
-	close(o);
+	buffer[reads] = '\0';
 
-	return (w);
+	text = write(STDOUT_FILENO, buffer, reads);
+	if ((text == -1) || (text != reads))
+	{
+		close(fdes);
+		free(buffer);
+		return (0);
+	}
+
+	close(fdes);
+	free(buffer);
+
+	return (reads);
 }
